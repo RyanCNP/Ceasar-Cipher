f5fac9d5c5235857bc1d3949d241d275173db69c
RyanCNP <ryan.cnp234@gmail.com>
2026-05-11 16:00:44 -0300

    feat: protegendo a rota para criptografia

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,20 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
 import { EducationComponent } from './features/education/education.component';
 import { CipherComponent } from './features/cipher/cipher.component';
+import { RegisterComponent } from './features/auth/register/register';
+import { LoginComponent } from './features/auth/login/login';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
-    {path: '', redirectTo: '/home', pathMatch: 'full'},
-    {path: 'home', component: HomeComponent},
-    {path: 'curiosidades', component: EducationComponent},
-    {path: 'cifra', component: CipherComponent}
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'curiosidades', component: EducationComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+
+  {
+    path: 'cifra',
+    component: CipherComponent,
+    canActivate: [authGuard]
+  }
 ];
