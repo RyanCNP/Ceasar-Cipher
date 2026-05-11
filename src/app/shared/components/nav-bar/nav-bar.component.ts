aec0fab104ab0e88b79c1b69a0aee28b883149a6
RyanCNP <ryan.cnp234@gmail.com>
2026-05-11 15:56:49 -0300

    feat: update navbar

--- src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Component} from '@angular/core';
+import { AuthService } from '../../../services/auth.service';
+import { Router, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterLink],
+  standalone: true,
   templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.css'
+  styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent {
+  constructor(private authService: AuthService, private router: Router) {}
 
+  isLoggedIn(): boolean {
+    return this.authService.isAuthenticated();
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }
