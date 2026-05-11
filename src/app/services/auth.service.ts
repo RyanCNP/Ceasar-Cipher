69c682478101cda147d98461cbeeacdc1075131a
RyanCNP <ryan.cnp234@gmail.com>
2026-05-11 16:01:47 -0300

    feat: authentication

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private readonly API_URL = 'http://seu-backend-aqui/api';
+  private loggedIn = new BehaviorSubject<boolean>(this.hasToken());
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  get isLoggedIn$(): Observable<boolean> {
+    return this.loggedIn.asObservable();
+  }
+
+  private hasToken(): boolean {
+    return !!localStorage.getItem('token');
+  }
+
+  login(credentials: any): Observable<any> {
+    return this.http.post(`${this.API_URL}/login`, credentials).pipe(
+      tap((response: any) => {
+        if (response.token) {
+          localStorage.setItem('token', response.token);
+          this.loggedIn.next(true);
+        }
+      })
+    );
+  }
+
+  register(user: any): Observable<any> {
+    return this.http.post(`${this.API_URL}/register`, user);
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.loggedIn.next(false);
+    this.router.navigate(['/login']);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem('token');
+  }
+
+  isAuthenticated(): boolean {
+    return this.hasToken();
+  }
+}
